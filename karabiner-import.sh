116320ebca83df279dd4864ea0d43c4bf5511a01
Max Rozentsveyg <mxr@users.noreply.github.com>
2017-07-31 13:14:13 -0700

    Create karabiner-import.sh

---
+++ karabiner-import.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set remap.consumer_to_fkeys_f5 1
+/bin/echo -n .
+$cli set remap.consumer_to_fkeys_f7 1
+/bin/echo -n .
+$cli set remap.escape2capslock 1
+/bin/echo -n .
+$cli set remap.fn_consumer_to_fkeys_f5 1
+/bin/echo -n .
+$cli set remap.fn_consumer_to_fkeys_f7 1
+/bin/echo -n .
+/bin/echo
